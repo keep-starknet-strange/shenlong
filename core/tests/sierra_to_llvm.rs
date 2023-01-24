557b304bd04d33e6caf006eb63af74123f19cff1
LucasLvy <lucaslevy10@gmail.com>
2023-01-24 12:51:29 +0100

    test(core): test addition compilation

--- core/tests/sierra_to_llvm.rs
+++ core/tests/sierra_to_llvm.rs
@@ -1,28 +1,26 @@
 extern crate tempdir;
+use shenlong_core::sierra::llvm_compiler::Compiler;
+use tempdir::TempDir;
+use test_case::test_case;
 
 /// Define a macro to get the path of a test resource file.
 macro_rules! test_resource_file {
     ($fname:expr) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/resources/test/", $fname) // assumes Linux ('/')!
+        format!("{}{}{}",env!("CARGO_MANIFEST_DIR"), "/tests/test_data/", $fname) // assumes Linux ('/')!
     };
 }
 
-#[cfg(test)]
-mod tests {
-    use shenlong_core::sierra::llvm_compiler::Compiler;
-    use tempdir::TempDir;
-
-    #[test]
-    fn compile_sierra_program_to_llvm() {
-        let program_path = test_resource_file!("sierra/simple_test.sierra");
-        let tmp_dir = TempDir::new("tmp").unwrap();
-        let output_path = tmp_dir.path().join("simple_test.ll");
-        let result = Compiler::compile_from_file(program_path, output_path.to_str().unwrap());
-        assert!(result.is_ok());
-        let _llvm_ir = std::fs::read_to_string(output_path).unwrap();
-        let _expected_llvm_ir =
-            std::fs::read_to_string(test_resource_file!("sierra/simple_test_expected.ll")).unwrap();
-        // assert_eq!(llvm_ir, expected_llvm_ir);
-        tmp_dir.close().unwrap();
-    }
+#[test_case("addition")]
+fn compile_sierra_program_to_llvm(name: &str) {
+    let program_path = test_resource_file!(format!("sierra/{}.sierra", name));
+    println!("{}", program_path);
+    let tmp_dir = TempDir::new("tmp").unwrap();
+    let output_path = tmp_dir.path().join("test.ll");
+    let result = Compiler::compile_from_file(program_path.as_str(), output_path.to_str().unwrap());
+    assert!(result.is_ok());
+    let llvm_ir = std::fs::read_to_string(output_path).unwrap();
+    let expected_llvm_ir =
+        std::fs::read_to_string(test_resource_file!(format!("llvm/{}.ll", name))).unwrap();
+    assert_eq!(llvm_ir, expected_llvm_ir);
+    tmp_dir.close().unwrap();
 }
