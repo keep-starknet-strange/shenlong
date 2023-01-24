f96e3778ce51f2fb9ce23eab5ac1ad5bb713cfc3
LucasLvy <LucasLvy@users.noreply.github.com>
2023-01-24 13:42:01 +0000

    deploy: 557b304bd04d33e6caf006eb63af74123f19cff1

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "shenlong":["",[],["lib.rs"]],\
 "shenlong_cli":["",[],["cli.rs","emoji.rs","lib.rs"]],\
-"shenlong_core":["",[["sierra",[],["llvm_compiler.rs","mod.rs"]]],["lib.rs"]]\
+"shenlong_core":["",[["sierra",[["libfunc",[],["mod.rs"]]],["llvm_compiler.rs","mod.rs"]]],["lib.rs"]]\
 }');
 createSourceSidebar();
