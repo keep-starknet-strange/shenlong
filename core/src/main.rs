3e3c1507f963191aa86621a021198f55b4df260a
LucasLvy <lucaslevy10@gmail.com>
2023-01-27 11:59:04 +0100

    fix(test): fix test expected llvm

--- core/src/main.rs
+++ core/src/main.rs
@@ -2,7 +2,7 @@ use shenlong_core::sierra::errors::CompilerResult;
 use shenlong_core::sierra::llvm_compiler;
 fn main() -> CompilerResult<()> {
     llvm_compiler::Compiler::compile_from_file(
-        "./core/tests/test_data/sierra/fib.sierra",
+        "./core/tests/test_data/sierra/addition.sierra",
         "./core/tests/test_data/llvm/test.ll",
     )?;
     Ok(())
