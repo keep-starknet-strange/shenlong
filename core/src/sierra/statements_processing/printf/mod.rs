5e60f1b2caa84ee5ccb8b5c5797b22ce664f1046
LucasLvy <lucaslevy10@gmail.com>
2023-02-16 15:04:00 +0100

    refact(core): change file names + add doc

--- core/src/sierra/statements_processing/printf/mod.rs
+++ core/src/sierra/statements_processing/printf/mod.rs
@@ -5,28 +5,28 @@ use inkwell::AddressSpace;
 use crate::sierra::llvm_compiler::Compiler;
 
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-    /// Implementation of the LLVM IR conversion of a struct construction operation.
-    ///
-    /// # Arguments
-    ///
-    /// * `libfunc_declaration` - The corelib function declaration of StructConstruct<T>.
-    ///
-    /// # Error
-    ///
-    /// Returns an error if the type T has not been declared previously.
+    /// Implementation of the LLVM IR wrapper for the printf function.
     pub fn printf(&self, ty: StructType<'ctx>) {
+        // declare i32 @printf(ptr, ...)
+        // i8* is the str type
         let i8_type = self.context.i8_type();
         let str_type = i8_type.ptr_type(AddressSpace::from(0));
+        // return type of printf
         let i32_type = self.context.i32_type();
+        // Define llvm signature of printf
         let printf_type = i32_type.fn_type(&[str_type.into()], true);
+        // Declare the function in the module.
         let printfunc = self.module.add_function("printf", printf_type, Some(Linkage::External));
+        // Wrapper of printf to call it with the expected main return value.
         let func = self.module.add_function("print", self.context.i32_type().fn_type(&[ty.into()], false), None);
         self.builder.position_at_end(self.context.append_basic_block(func, "entry"));
+        // printf prefix needed ("%d")
         let prefix_ptr = self.builder.build_alloca(i8_type.array_type(2), "prefix");
         self.builder.build_store(
             prefix_ptr,
             self.context.i8_type().const_array(&[i8_type.const_int(37, false), i8_type.const_int(100, false)]),
         );
+        // Return if print has worked.
         let res = self
             .builder
             .build_call(
