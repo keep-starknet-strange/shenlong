64d941ed47144faf600c794340de956fe5d44883
LucasLvy <lucaslevy10@gmail.com>
2023-02-20 12:12:15 +0100

    fix(core): fix fib test

--- core/src/main.rs
+++ core/src/main.rs
@@ -4,9 +4,9 @@ use shenlong_core::sierra::errors::CompilerResult;
 use shenlong_core::sierra::llvm_compiler;
 fn main() -> CompilerResult<()> {
     llvm_compiler::Compiler::compile_from_file(
-        Path::new("./core/tests/test_data/sierra/fib_bench.sierra"),
-        Path::new("./core/tests/test_data/llvm/ll/fib_bench.ll"),
-        Path::new("./core/tests/test_data/llvm/bc/fib_bench.bc"),
+        Path::new("./core/tests/test_data/sierra/fib.sierra"),
+        Path::new("./core/tests/test_data/llvm/ll/fib.ll"),
+        Path::new("./core/tests/test_data/llvm/bc/fib.bc"),
     )?;
     Ok(())
 }
