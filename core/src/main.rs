28c974e6d3e80a9f503c6b13ab2799127c078aab
LucasLvy <lucaslevy10@gmail.com>
2023-02-20 18:24:09 +0100

    refacto(bench): switch to compiling the sierra file in the bench

--- core/src/main.rs
+++ core/src/main.rs
@@ -6,7 +6,6 @@ fn main() -> CompilerResult<()> {
     llvm_compiler::Compiler::compile_from_file(
         Path::new("./core/tests/test_data/sierra/fib_box_main.sierra"),
         Path::new("./core/tests/test_data/llvm/ll/fib_box_main.ll"),
-        Path::new("./core/tests/test_data/llvm/bc/fib_box_main.bc"),
         Some("x86_64-pc-linux-gnu"),
     )?;
     Ok(())
