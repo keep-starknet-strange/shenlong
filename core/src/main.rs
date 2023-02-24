f9c3850e7c7515afd95b21e29fec2bb59a21cf0b
LucasLvy <lucaslevy10@gmail.com>
2023-02-24 21:44:27 +0100

    fix(bench): upload updated bench version

--- core/src/main.rs
+++ core/src/main.rs
@@ -4,8 +4,8 @@ use shenlong_core::sierra::errors::CompilerResult;
 use shenlong_core::sierra::llvm_compiler;
 fn main() -> CompilerResult<()> {
     llvm_compiler::Compiler::compile_from_file(
-        Path::new("./core/tests/test_data/sierra/fib_box_main.sierra"),
-        Path::new("./core/tests/test_data/llvm/ll/fib_box_main.ll"),
+        Path::new("./core/tests/test_data/sierra/fib_bench.sierra"),
+        Path::new("./core/tests/test_data/llvm/fib_bench.ll"),
         Some("x86_64-pc-linux-gnu"),
     )?;
     Ok(())
