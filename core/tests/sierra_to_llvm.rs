3289eb37fe1a0814fcba9b01ecb2e049c12fb482
Edgar Luque <git@edgarluque.com>
2023-02-28 19:59:04 +0100

    make tests debug path agnostic

--- core/tests/sierra_to_llvm.rs
+++ core/tests/sierra_to_llvm.rs
@@ -1,4 +1,5 @@
 extern crate tempdir;
+use regex::Regex;
 use shenlong_core::sierra::llvm_compiler::Compiler;
 use tempdir::TempDir;
 use test_case::test_case;
@@ -22,8 +23,15 @@ fn compile_sierra_program_to_llvm(name: &str) {
     let llvm_output_path = tmp_dir.path().join("test.ll");
     let result = Compiler::compile_from_file(&program_path, &llvm_output_path, Some("x86_64-pc-linux-gnu"));
     assert!(result.is_ok());
-    let llvm_ir = std::fs::read_to_string(llvm_output_path).unwrap();
-    let expected_llvm_ir = std::fs::read_to_string(test_resource_file!(format!("llvm/{}.ll", name))).unwrap();
+    let mut llvm_ir = std::fs::read_to_string(llvm_output_path).unwrap();
+    let mut expected_llvm_ir = std::fs::read_to_string(test_resource_file!(format!("llvm/{}.ll", name))).unwrap();
+
+    // Replace the debug path.
+    let debug_filename_regex = Regex::new(r#"!DIFile\(filename: ".*", directory: ".*"\)"#).unwrap();
+    llvm_ir = debug_filename_regex.replace(&llvm_ir, r#"!DIFile(filename: "", directory: "")"#).to_string();
+    expected_llvm_ir =
+        debug_filename_regex.replace(&expected_llvm_ir, r#"!DIFile(filename: "", directory: "")"#).to_string();
+
     pretty_assertions::assert_eq!(llvm_ir, expected_llvm_ir);
     tmp_dir.close().unwrap();
 }
