333162d8101a38b135d67dd8573a10fd89007b02
Edgar <git@edgarluque.com>
2023-03-01 13:42:12 +0100

    fix conflict

--- core/src/sierra/process/corelib.rs
+++ core/src/sierra/process/corelib.rs
@@ -68,6 +68,8 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                 "felt_mul" => self.felt_mul(libfunc_declaration),
                 // Subtraction for the felt type. `felt - felt`
                 "felt_sub" => self.felt_sub(libfunc_declaration),
+                // Division for the felt type. `felt / NonZero<felt>`
+                "felt_div" => self.felt_div(libfunc_declaration),
                 // Calls a user defined sierra function. `function_call<user@fib_caller::fib_caller::fib>`
                 "function_call" => debug!(libfunc_name, "treated in the statements"),
                 // Boxes a value of type T.
