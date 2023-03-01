0595a8bb39750c5941dbaecf58784364964f92c2
Edgar <git@edgarluque.com>
2023-03-01 13:01:10 +0100

    feat(test): proptests pass for add,sub,mul

--- core/tests/proptests.rs
+++ core/tests/proptests.rs
@@ -1,23 +1,12 @@
+use std::ops::*;
 use std::process::{Command, Stdio};
 
 use num_bigint::BigInt;
-use num_traits::Num;
-use pretty_assertions::assert_eq;
+use num_traits::{Num, Zero};
 use proptest::prelude::*;
 use serde::Serialize;
 use shenlong_core::sierra::llvm_compiler::Compiler;
 use tinytemplate::TinyTemplate;
-
-macro_rules! test_template_file {
-    ($template_file:literal, $ctx:ident) => {{
-        let template =
-            std::path::PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/templates/", $template_file));
-        let mut tt = TinyTemplate::new();
-        let template = std::fs::read_to_string(template).unwrap();
-        tt.add_template("test", &template).unwrap();
-        tt.render("test", &$ctx).unwrap().clone()
-    }};
-}
 
 #[derive(Serialize)]
 struct BinaryContext<'a> {
@@ -29,12 +18,24 @@ fn get_prime() -> BigInt {
     BigInt::from_str_radix("3618502788666131213697322783095070105623107215331596699973092056135872020481", 10).unwrap()
 }
 
-#[test]
-fn simple_addition() {
-    let ctx = BinaryContext { lhs: "9223372036854775807", rhs: "9223372036854775807" };
-    let source = test_template_file!("addition.sierra", ctx);
+fn test_binary_op<F>(
+    template_name: &str,
+    lhs: &str,
+    rhs: &str,
+    bigint_op: F,
+) -> Result<(), proptest::test_runner::TestCaseError>
+where
+    F: FnOnce(BigInt, BigInt) -> BigInt,
+{
+    let ctx = BinaryContext { lhs, rhs };
+    let template =
+        std::path::PathBuf::from(format!("{}/tests/templates/{}", env!("CARGO_MANIFEST_DIR"), template_name));
+    let mut tt = TinyTemplate::new();
+    let template = std::fs::read_to_string(template).unwrap();
+    tt.add_template("test", &template).unwrap();
+    let source = tt.render("test", &ctx).unwrap();
 
-    let tmp = tempdir::TempDir::new("test_simple_addition").unwrap();
+    let tmp = tempdir::TempDir::new("shenlong_tests").unwrap();
     let file = tmp.into_path().join("output.ll");
 
     Compiler::compile_from_code(&source, &file, None).unwrap();
@@ -48,86 +49,40 @@ fn simple_addition() {
 
     assert!(output.starts_with("Return value: "));
     let output = &output["Return value: ".len()..];
-    assert_eq!(output, "000000000000000000000000000000000000000000000000FFFFFFFFFFFFFFFE");
 
-    let x = BigInt::from_str_radix(output, 16).unwrap();
+    let return_value = BigInt::from_str_radix(output, 16).unwrap();
 
     let prime = get_prime();
 
-    let a = BigInt::from_str_radix("9223372036854775807", 10).unwrap();
-    let b = BigInt::from_str_radix("9223372036854775807", 10).unwrap();
-    let c = a + b;
-    let expected = c % prime;
+    let lhs = BigInt::from_str_radix(lhs, 10).unwrap();
+    let rhs = BigInt::from_str_radix(rhs, 10).unwrap();
+    let mut expected = bigint_op(lhs, rhs) % prime;
+    let zero = BigInt::zero();
+    let two = BigInt::from(2).pow(return_value.bits() as u32);
+    expected += if expected < zero { two } else { zero };
+    prop_assert_eq!(return_value, expected);
+    Ok(())
+}
 
-    assert_eq!(x, expected);
+#[test]
+fn simple_addition() {
+    test_binary_op("addition.sierra", "9223372036854775807", "9223372036854775807", BigInt::add).unwrap();
 }
 
 #[test]
 fn addition_overflow() {
-    let ctx =
-        BinaryContext { lhs: "3618502788666131213697322783095070105623107215331596699973092056135872020480", rhs: "2" };
-    let source = test_template_file!("addition.sierra", ctx);
-
-    let tmp = tempdir::TempDir::new("test_simple_addition").unwrap();
-    let file = tmp.into_path().join("output.ll");
-
-    Compiler::compile_from_code(&source, &file, None).unwrap();
-
-    let lli_path = std::env::var("LLI_PATH").expect("LLI_PATH must exist and point to the `lli` tool from llvm 16");
-
-    let cmd = Command::new(lli_path).arg(file).stdout(Stdio::piped()).spawn().unwrap();
-
-    let output = cmd.wait_with_output().unwrap();
-    let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-
-    assert!(output.starts_with("Return value: "));
-    let output = &output["Return value: ".len()..];
-    assert_eq!(output, "0000000000000000000000000000000000000000000000000000000000000001");
-
-    let x = BigInt::from_str_radix(output, 16).unwrap();
-
-    let prime = get_prime();
-
-    let a = BigInt::from_str_radix("3618502788666131213697322783095070105623107215331596699973092056135872020480", 10)
-        .unwrap();
-    let b = BigInt::from_str_radix("2", 10).unwrap();
-    let c = a + b;
-    let expected = c % prime;
-
-    assert_eq!(x, expected);
+    test_binary_op(
+        "addition.sierra",
+        "3618502788666131213697322783095070105623107215331596699973092056135872020480",
+        "2",
+        BigInt::add,
+    )
+    .unwrap();
 }
 
 #[test]
 fn substraction_negative_result() {
-    let ctx = BinaryContext { lhs: "2", rhs: "4" };
-    let source = test_template_file!("substraction.sierra", ctx);
-
-    let tmp = tempdir::TempDir::new("test_simple_addition").unwrap();
-    let file = tmp.into_path().join("output.ll");
-
-    Compiler::compile_from_code(&source, &file, None).unwrap();
-
-    let lli_path = std::env::var("LLI_PATH").expect("LLI_PATH must exist and point to the `lli` tool from llvm 16");
-
-    let cmd = Command::new(lli_path).arg(file).stdout(Stdio::piped()).spawn().unwrap();
-
-    let output = cmd.wait_with_output().unwrap();
-    let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-
-    assert!(output.starts_with("Return value: "));
-    let output = &output["Return value: ".len()..];
-    assert_eq!(output, "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE");
-
-    let x = BigInt::from_str_radix(output, 16).unwrap();
-
-    let prime = get_prime();
-
-    let a = BigInt::from_str_radix("2", 10).unwrap();
-    let b = BigInt::from_str_radix("4", 10).unwrap();
-    let expected = a - b;
-    let expected = expected % prime;
-
-    assert_eq!(x, expected);
+    test_binary_op("substraction.sierra", "2", "4", BigInt::sub).unwrap();
 }
 
 proptest! {
@@ -135,92 +90,20 @@ proptest! {
     fn proptest_add(a: i64, b: i64) {
         let lhs = a.to_string();
         let rhs = b.to_string();
-        let ctx = BinaryContext { lhs: &lhs, rhs: &rhs};
-        let source = test_template_file!("addition.sierra", ctx);
-
-        let tmp = tempdir::TempDir::new("shenlog_test_add").unwrap();
-        let file = tmp.into_path().join("output.ll");
-
-        Compiler::compile_from_code(&source, &file, None).unwrap();
-
-        let lli_path = std::env::var("LLI_PATH").expect("LLI_PATH must exist and point to the `lli` tool from llvm 16");
-
-        let cmd = Command::new(lli_path).arg(file).stdout(Stdio::piped()).spawn().unwrap();
-
-        let output = cmd.wait_with_output().unwrap();
-        let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-
-        prop_assert!(output.starts_with("Return value: "));
-        let output = &output["Return value: ".len()..];
-
-        let x = BigInt::from_str_radix(output, 16).unwrap();
-
-        let a = BigInt::from_str_radix(&lhs, 10).unwrap();
-        let b = BigInt::from_str_radix(&rhs, 10).unwrap();
-        let c = a + b;
-
-        prop_assert_eq!(x, c);
+        test_binary_op("addition.sierra", &lhs, &rhs, BigInt::add)?;
     }
 
     #[test]
     fn proptest_sub(a: i64, b: i64) {
         let lhs = a.to_string();
         let rhs = b.to_string();
-        let ctx = BinaryContext { lhs: &lhs, rhs: &rhs};
-        let source = test_template_file!("substraction.sierra", ctx);
-
-        let tmp = tempdir::TempDir::new("shenlog_test_sub").unwrap();
-        let file = tmp.into_path().join("output.ll");
-
-        Compiler::compile_from_code(&source, &file, None).unwrap();
-
-        let lli_path = std::env::var("LLI_PATH").expect("LLI_PATH must exist and point to the `lli` tool from llvm 16");
-
-        let cmd = Command::new(lli_path).arg(file).stdout(Stdio::piped()).spawn().unwrap();
-
-        let output = cmd.wait_with_output().unwrap();
-        let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-
-        prop_assert!(output.starts_with("Return value: "));
-        let output = &output["Return value: ".len()..];
-
-        let x = BigInt::from_str_radix(output, 16).unwrap();
-
-        let a = BigInt::from_str_radix(&lhs, 10).unwrap();
-        let b = BigInt::from_str_radix(&rhs, 10).unwrap();
-        let c = a - b;
-
-        prop_assert_eq!(x, c);
+        test_binary_op("substraction.sierra", &lhs, &rhs, BigInt::sub)?;
     }
 
     #[test]
     fn proptest_mul(a: i64, b: i64) {
         let lhs = a.to_string();
         let rhs = b.to_string();
-        let ctx = BinaryContext { lhs: &lhs, rhs: &rhs};
-        let source = test_template_file!("mul.sierra", ctx);
-
-        let tmp = tempdir::TempDir::new("shenlog_test_mul").unwrap();
-        let file = tmp.into_path().join("output.ll");
-
-        Compiler::compile_from_code(&source, &file, None).unwrap();
-
-        let lli_path = std::env::var("LLI_PATH").expect("LLI_PATH must exist and point to the `lli` tool from llvm 16");
-
-        let cmd = Command::new(lli_path).arg(file).stdout(Stdio::piped()).spawn().unwrap();
-
-        let output = cmd.wait_with_output().unwrap();
-        let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-
-        prop_assert!(output.starts_with("Return value: "));
-        let output = &output["Return value: ".len()..];
-
-        let x = BigInt::from_str_radix(output, 16).unwrap();
-
-        let a = BigInt::from_str_radix(&lhs, 10).unwrap();
-        let b = BigInt::from_str_radix(&rhs, 10).unwrap();
-        let c = a * b;
-
-        prop_assert_eq!(x, c);
+        test_binary_op("mul.sierra", &lhs, &rhs, BigInt::mul)?;
     }
 }
