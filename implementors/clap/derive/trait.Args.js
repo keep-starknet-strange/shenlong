07744e763a5ce3cc78f5a5e16efe7ed6849f8f84
LucasLvy <LucasLvy@users.noreply.github.com>
2023-03-03 11:26:21 +0000

    deploy: 4ecd9e05add26f62798a9f0fdee3294bbb16111f

--- implementors/clap/derive/trait.Args.js
+++ implementors/clap/derive/trait.Args.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"shenlong":[["impl Args for <a class=\"struct\" href=\"shenlong/cli/struct.SierraCommands.html\" title=\"struct shenlong::cli::SierraCommands\">SierraCommands</a>"],["impl Args for <a class=\"struct\" href=\"shenlong/cli/struct.Command.html\" title=\"struct shenlong::cli::Command\">Command</a>"]]
+"shenlong":[["impl Args for <a class=\"struct\" href=\"shenlong/cli/struct.Command.html\" title=\"struct shenlong::cli::Command\">Command</a>"],["impl Args for <a class=\"struct\" href=\"shenlong/cli/struct.SierraCommands.html\" title=\"struct shenlong::cli::SierraCommands\">SierraCommands</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
