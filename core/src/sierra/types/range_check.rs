1aede56c87f57a77cad075f3800cc0729925ccd4
LucasLvy <lucaslevy10@gmail.com>
2023-03-06 17:00:00 +0100

    fix(core): add debug info for rangecheck

--- core/src/sierra/types/range_check.rs
+++ core/src/sierra/types/range_check.rs
@@ -6,6 +6,7 @@ use crate::sierra::llvm_compiler::Compiler;
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
     pub fn range_check(&mut self, type_declaration: &TypeDeclaration) {
         let ty = self.context.custom_width_int_type(0).as_basic_type_enum();
+        self.debug.create_type(type_declaration.id.id, "RangeCheck", 0);
         self.types_by_name.insert("RangeCheck".to_owned(), ty);
         self.types_by_id.insert(type_declaration.id.id, ty);
     }
